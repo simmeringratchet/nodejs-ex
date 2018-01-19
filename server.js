ef9c9b0ed09e134003c534fed65c569528f3ed27
sicain <sicain@cisco.com>
2018-01-19 16:18:00 +0000

    New env variable logging

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
+console.log('Value of ONE_THING: %s', process.env.ONE_THING || 'Unknown');
+
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
